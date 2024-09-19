4603ce9655c2bac26a315fd80db1f02b7585b58d
mazzzystar <2680461921@qq.com>
2024-09-19 17:47:57 +0800

    change padding.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ThoughtsList from "@/components/ThoughtsList";
 export default function Home() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
-      <main className="flex-grow py-8 pt-20">
+      <main className="flex-grow py-8 pt-10 sm:pt-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <ThoughtsList />
         </div>
