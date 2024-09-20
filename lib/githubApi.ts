fa2c51c306d9b8745387410b57797a862f12289d
mazzzystar <2680461921@qq.com>
2024-09-20 18:33:00 +0800

    fix blog title chinese name issue.

--- lib/githubApi.ts
+++ lib/githubApi.ts
@@ -216,7 +216,7 @@ export async function getBlogPost(id: string, accessToken: string): Promise<Blog
     const contentResponse = await octokit.repos.getContent({
       owner,
       repo,
-      path: `content/blog/${id}.md`,
+      path: `content/blog/${decodeURIComponent(id)}.md`,
     });
 
     if (Array.isArray(contentResponse.data) || !('content' in contentResponse.data)) {
@@ -229,7 +229,7 @@ export async function getBlogPost(id: string, accessToken: string): Promise<Blog
     const commitResponse = await octokit.repos.listCommits({
       owner,
       repo,
-      path: `content/blog/${decodeURIComponent(id)}.mdd`,
+      path: `content/blog/${decodeURIComponent(id)}.md`,
       per_page: 1
     });
 
